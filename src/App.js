504274c5bce131a947a78b58f6a098bac8a03e66
Seung876 <sm08073469@gmail.com>
2025-05-11 12:39:52 +0900

    파일명 수정

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Zero from './pages/Zero';
 import Main from './pages/Main';
-import Intro from './pages/Intro';
+import IModify from './pages/IModify';
 import Match from './pages/Match';
 import Manage from './pages/Manage';
-import Modify from './pages/Modify';
+import TModify from './pages/TModify';
 import Create from './pages/Create';
 import Chat from './pages/Chat';
 import My from './pages/My';
@@ -25,10 +25,10 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/zero" element={<Zero />} />
         <Route path="/main" element={<Main />} />
-        <Route path="/intro" element={<Intro />} />
+        <Route path="/imodify" element={<IModify />} />
         <Route path="/match" element={<Match />} />
         <Route path="/manage" element={<Manage />} />
-        <Route path="/modify" element={<Modify />} />
+        <Route path="/tmodify" element={<TModify />} />
         <Route path="/create" element={<Create />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/my" element={<My />} />
---
+++ src/pages/IModify/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function IModify() {
+  const element = <h1>회원정보 수정페이지</h1>;
+  return element;
+}
+
+export default IModify;
