b2ce1b18161163e6916fb2a9f4478c9f0e553470
Yujun Lee <yjlove2089@gmail.com>
2025-05-21 04:13:46 +0900

    팀 관리 페이지 구조 및 스타일 구현

--- src/pages/TModify/index.jsx
+++ src/pages/TModify/index.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import TeamModifyForm from '../../components/TModify/TeamModifyForm';
+import './ModifyPage.css';
 
-function TModify() {
-  const element = <h1>팀 수정 페이지</h1>;
-  return element;
+function TModifyPage() {
+  const teams = [
+    {
+      id: 1,
+      name: '솔로지옥 시즌 1',
+      description: '저희는 이게 매력적 입니다~!',
+      quote: '진짜 재밌다 ㅋ'
+    },
+    {
+      id: 2,
+      name: '솔로지옥 시즌 2',
+      description: '저희는 이게 매력적 입니다~!',
+      quote: '진짜 재밌다 ㅋ'
+    },
+    {
+      id: 3,
+      name: '솔로지옥 시즌 3',
+      description: '저희는 이게 매력적 입니다~!',
+      quote: '진짜 재밌다 ㅋ'
+    }
+  ];
+
+  return (
+    <div className="manage-wrapper" style={{ paddingTop: '24px', paddingBottom: '100px' }}>
+      <Header />
+      <main className="manage-container">
+        <div className="edit-wrapper">
+          <button className="edit-btn">팀 수정하기</button>
+        </div>
+        {teams.map((team) => (
+          <TeamModifyForm key={team.id} team={team} />
+        ))}
+        <button className="create-btn">수정하기</button>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default TModify;
+export default TModifyPage;
