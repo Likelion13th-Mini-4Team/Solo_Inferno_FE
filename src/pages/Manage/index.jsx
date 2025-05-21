8bdbbb24ac7f85a1884a835e101830b0d5cfc548
Yujun Lee <yjlove2089@gmail.com>
2025-05-21 14:51:42 +0900

    fix: 충돌 해결 (ManagePage)

--- src/pages/Manage/index.jsx
+++ src/pages/Manage/index.jsx
@@ -10,20 +10,20 @@ function ManagePage() {
       id: 1,
       name: '솔로지옥 시즌 1',
       description: '저희는 이게 매력적 입니다~!',
-      quote: '진짜 재밌다 ㅋ'
+      quote: '진짜 재밌다 ㅋ',
     },
     {
       id: 2,
       name: '솔로지옥 시즌 2',
       description: '저희는 이게 매력적 입니다~!',
-      quote: '진짜 재밌다 ㅋ'
+      quote: '진짜 재밌다 ㅋ',
     },
     {
       id: 3,
       name: '솔로지옥 시즌 3',
       description: '저희는 이게 매력적 입니다~!',
-      quote: '진짜 재밌다 ㅋ'
-    }
+      quote: '진짜 재밌다 ㅋ',
+    },
   ];
 
   return (
@@ -33,9 +33,11 @@ function ManagePage() {
         <div className="edit-wrapper">
           <button className="edit-btn">팀 수정하기</button>
         </div>
+
         {teams.map((team) => (
           <TeamManageForm key={team.id} team={team} />
         ))}
+
         <button className="create-btn">팀 생성하기</button>
       </main>
       <Footer />
